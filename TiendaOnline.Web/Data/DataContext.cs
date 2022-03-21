3e4fa907928d6562cef19698fd65109b47e3eb18
FABIO ALEXANDER CANO MARULANDA <fabiocano219609@correo.itm.edu.co>
2022-03-21 10:32:54 -0500

    Add settings for sql conection

--- TiendaOnline.Web/Data/DataContext.cs
+++ TiendaOnline.Web/Data/DataContext.cs
@@ -1,12 +1,22 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using TiendaOnline.Common.Entities;
 
 namespace TiendaOnline.Web.Data
 {
     public class DataContext : DbContext
     {
-        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        public DataContext(DbContextOptions<DataContext> options) : base(options){}
+
+        public DbSet<Country> Countries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
 
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Country>()
+                .HasIndex(t => t.name)
+                .IsUnique();
         }
     }
 }
