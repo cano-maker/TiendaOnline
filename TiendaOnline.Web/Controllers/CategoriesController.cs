bb26727e51af50fff93485bd61f6c28e9593965f
Fabio Cano <fabiocano894@gmail.com>
2022-04-14 20:15:53 -0500

    add categories controller

---
+++ TiendaOnline.Web/Controllers/CategoriesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using TiendaOnline.Web.Data;
+
+namespace TiendaOnline.Web.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private readonly DataContext _context;
+        public CategoriesController(DataContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Categories.ToListAsync());
+        }
+
+    }
+}
