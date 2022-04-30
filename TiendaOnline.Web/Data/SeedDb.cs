98b83f1d7b853b9e8d096cdd227794fe32290bd4
Fabio Cano <fabio.cano@sofka.com.co>
2022-04-30 14:06:12 -0500

    add categories in the seeder

--- TiendaOnline.Web/Data/SeedDb.cs
+++ TiendaOnline.Web/Data/SeedDb.cs
@@ -8,14 +8,25 @@ namespace TiendaOnline.Web.Data
     public class SeedDb
     {
         private readonly DataContext _context;
-        public SeedDb(DataContext context)
-        {
-            _context = context;
-        }
+        public SeedDb(DataContext context) { _context = context; }
         public async Task SeedAsync()
         {
             await _context.Database.EnsureCreatedAsync();
             await CheckCountriesAsync();
+            await CheckCategoriesAsync();
+        }
+
+        private async Task CheckCategoriesAsync()
+        {
+            if (!_context.Categories.Any())
+            {
+                _context.Categories.Add(new Category { Name = "Tecnología" });
+                _context.Categories.Add(new Category { Name = "Ropa" });
+                _context.Categories.Add(new Category { Name = "Gamer" });
+                _context.Categories.Add(new Category { Name = "Belleza" });
+                _context.Categories.Add(new Category { Name = "Nutrición" });
+            }
+            await _context.SaveChangesAsync();
         }
         private async Task CheckCountriesAsync()
         {
@@ -24,86 +35,70 @@ namespace TiendaOnline.Web.Data
                 _context.Countries.Add(new Country
                 {
                     Name = "Colombia",
-                    Departments = new List<Department>
- {
- new Department
- {
- Name = "Antioquia",
- Cities = new List<City>
- {
- new City { Name = "Medellín" },
- new City { Name = "Envigado" },
- new City { Name = "Itagüí" }
- }
- },
- new Department
+                    Departments = new List<Department>(){
+                new Department()
 
-{
- Name = "Bogotá",
- Cities = new List<City>
+                    {Name = "Antioquia", Cities =
+                                             new List<City>(){
+                                                 new City(){Name = "Medellín"},
+                                                 new City(){Name = "Itagüí"},
+                                                 new City(){Name = "Envigado"},
+                                                 new City(){Name = "Bello"},
+                                                 new City(){Name = "Rionegro"},
 
-{
- new City { Name = "Bogotá" }
+                                             }},
+                new Department()
 
-}
- },
- new Department
+                    {Name = "Bogotá", Cities =
+                                          new List<City>(){
+                                              new City(){Name = "Usaquen"},
+                                              new City(){Name = "Champinero"},
+                                              new City(){Name = "Santa fe"},
+                                              new City(){Name = "Useme"},
+                                              new City(){Name = "Bosa"},
 
-{
- Name = "Valle del Cauca",
- Cities = new List<City>
-
-{
- new City { Name = "Calí" },
- new City { Name = "Buenaventura" },
- new City { Name = "Palmira" }
-
-}
-
-}
-
-}
-                });
-                _context.Countries.Add(new Country
-
-                {
-                    Name = "USA",
-                    Departments = new List<Department>
-
-{
- new Department
-
-{
- Name = "California",
- Cities = new List<City>
-
-{
- new City { Name = "Los Angeles" },
- new City { Name = "San Diego" },
- new City { Name = "San Francisco" }
-
-}
- },
- new Department
-
-{
- Name = "Illinois",
- Cities = new List<City>
-
-{
- new City { Name = "Chicago" },
- new City { Name = "Springfield" }
-
-}
-
-}
-
-}
-                });
-                await _context.SaveChangesAsync();
+                                          }},
 
             }
+                });
+                _context.Countries.Add(
+                    new Country
 
+                    {
+                        Name = "Estados Unidos",
+                        Departments = new List<Department>()
+
+                         {
+                     new Department()
+
+                         {Name = "Florida",
+                          Cities =
+                              new List<City>(){
+                                  new City(){Name = "Orlando"},
+                                  new City(){Name = "Miami"},
+                                  new City(){Name = "Tampa"},
+                                  new City(){Name = "Fort Lauderdale"},
+                                  new City(){Name = "Key West"},
+
+                              }},
+                     new Department()
+
+                         {Name = "Texas",
+                          Cities =
+                              new List<City>(){
+                                  new City(){Name = "Houston"},
+                                  new City(){Name = "San Antonio"},
+                                  new City(){Name = "Dallas"},
+                                  new City(){Name = "Austin"},
+                                  new City(){Name = "El Paso"},
+
+                              }},
+
+                         }
+                    });
+            }
+
+            await _context.SaveChangesAsync();
         }
     }
 
