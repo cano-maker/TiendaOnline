8aaf48be6689b031d188c36794004ab1a88524af
Fabio Cano <fabio.cano@sofka.com.co>
2022-05-01 13:17:27 -0500

    add recover password

--- TiendaOnline.Web/Helpers/UserHelper.cs
+++ TiendaOnline.Web/Helpers/UserHelper.cs
@@ -122,6 +122,16 @@ namespace TiendaOnline.Web.Helpers
             return await _userManager.GenerateEmailConfirmationTokenAsync(user);
         }
 
+        public async Task<string> GeneratePasswordResetTokenAsync(User user)
+        {
+            return await _userManager.GeneratePasswordResetTokenAsync(user);
+        }
+        public async Task<IdentityResult> ResetPasswordAsync(User user, string token, string password)
+        {
+            return await _userManager.ResetPasswordAsync(user, token, password);
+        }
+
+
     }
 
 }
--- TiendaOnline.Web/Interfaces/IUserHelper.cs
+++ TiendaOnline.Web/Interfaces/IUserHelper.cs
@@ -21,6 +21,8 @@ namespace TiendaOnline.Web.Interfaces
         Task<User> GetUserAsync(Guid userId);
         Task<string> GenerateEmailConfirmationTokenAsync(User user);
         Task<IdentityResult> ConfirmEmailAsync(User user, string token);
+        Task<string> GeneratePasswordResetTokenAsync(User user);
+        Task<IdentityResult> ResetPasswordAsync(User user, string token, string password);
 
 
     }
